ebb05c601b0efbf0bbbfdfe290cbb4e56366d9f3
linusHche <linushung28@gmail.com>
2018-09-24 14:48:58 -0700

    Fixed swap button and implementation of the unit conversion class

--- app/src/main/java/com/example/lda/simple_calculator/UnitConverter.java
+++ app/src/main/java/com/example/lda/simple_calculator/UnitConverter.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
+import java.text.DecimalFormat;
+
 public class UnitConverter extends AppCompatActivity {
 
     @Override
@@ -12,20 +14,19 @@ public class UnitConverter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unit_converter);
 
-        Spinner beforeSpinner = findViewById(R.id.before);
-        Spinner afterSpinner = findViewById(R.id.after);
+
+        final Spinner beforeSpinner = findViewById(R.id.before);
+        final Spinner afterSpinner = findViewById(R.id.after);
         final TextView display = findViewById(R.id.resultValue);
         Button convertButton = findViewById(R.id.convert_btn);
+        Button swapButton = findViewById(R.id.swap_btn);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(UnitConverter.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.units));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(UnitConverter.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.units));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         beforeSpinner.setAdapter(myAdapter);
-        afterSpinner.setAdapter(myAdapter);
-        EditText entry = findViewById(R.id.enterValue);
-        final double value = Double.parseDouble(entry.getText().toString());
-
-        final String beforeUnit = beforeSpinner.getSelectedItem().toString();
-        final String afterUnit = afterSpinner.getSelectedItem().toString();
-
+        afterSpinner.setAdapter(myAdapter2);
+        final EditText entry = findViewById(R.id.enterValue);
 
 
 
@@ -35,9 +36,28 @@ public class UnitConverter extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                double result = mediator(value, beforeUnit);
-                result = conversion(result, afterUnit);
-                display.setText(result+"");
+                try {
+                    String beforeUnit = beforeSpinner.getSelectedItem().toString();
+                    String afterUnit = afterSpinner.getSelectedItem().toString();
+                    double value = Double.parseDouble(entry.getText().toString());
+                    double result = mediator(value, beforeUnit);
+                    result = conversion(result, afterUnit);
+                    DecimalFormat numberFormat = new DecimalFormat("#.00000");
+                    display.setText(numberFormat.format(result));
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), " Please enter a valid input ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        swapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int beforePos = beforeSpinner.getSelectedItemPosition();
+                int afterPos = afterSpinner.getSelectedItemPosition();
+                beforeSpinner.setSelection(afterPos);
+                afterSpinner.setSelection(beforePos);
+
             }
         });
 
