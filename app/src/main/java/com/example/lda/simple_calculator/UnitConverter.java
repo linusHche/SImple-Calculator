a88426edc486350bda5579dbf71c61c327afa7ff
linusHche <linushung28@gmail.com>
2018-09-24 11:58:19 -0700

    Initialized UnitConverter activity

---
+++ app/src/main/java/com/example/lda/simple_calculator/UnitConverter.java
@@ -0,0 +1,13 @@
+package com.example.lda.simple_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnitConverter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_converter);
+    }
+}
